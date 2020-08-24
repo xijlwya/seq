4a975c7a41f0ee7b883b1b7a8358c3b0260f6771
xijlwya <ravenfrost@gmx.net>
2020-08-24 23:55:42 +0200

    Removes Sequence, added unittests, etc.

--- seq.py
+++ seq.py
@@ -1,250 +1,12 @@
 import mido
 import time
 import random
-import collections.abc
 import threading
 
-note_names = {
-	'c':60,
-	'd':62,
-	'e':64,
-	'f':65,
-	'g':67,
-	'a':69,
-	'b':71
-	}
+from seq_data import *
+#imports several note value dicts and CONSTANTS
 
-chord_names = {
-	'min'		:(0, 3, 7),
-	'maj'		:(0, 4, 7),
-	'min7'		:(0, 3, 7, 10),
-	'7'			:(0, 4, 7, 10),
-	'minmaj7'	:(0, 3, 7, 11),
-	'maj7'		:(0, 4, 7, 11),
-	'dim'		:(0, 3, 6),
-	'dim7'		:(0, 3, 6, 9),
-	'dimmin7'	:(0, 3, 6, 10),
-	'sus2'		:(0, 2, 7),
-	'7sus2'		:(0, 2, 7, 10),
-	'sus4'		:(0, 5, 7),
-	'7sus4'		:(0, 5,	7, 10),
-	'6'			:(0, 4, 7, 9),
-	'min6'		:(0, 3, 7, 9),
-	'b5'		:(0, 4, 6),
-	'9'			:(0, 4, 7, 11, 14),
-	'min9'		:(0, 3, 7, 10, 14),
-	'11'		:(0, 4, 7, 11, 14, 17),
-	'min11'		:(0, 3, 7, 10, 14, 17),
-	'aug'		:(0, 4, 8),
-	'#5'		:(0, 4, 8),
-	'augmaj7'	:(0, 4, 8, 11),
-	'aug7'		:(0, 4, 8, 10),
-}
-
-scale_names = {
-	'lydian'	:(0, 2, 4, 5, 7, 9, 10),
-	'ionian'	:(0, 2, 4, 5, 7, 9, 11),
-	'mixolydian':(0, 2, 4, 5, 7, 9, 10),
-	'dorian'	:(0, 2, 3, 5, 7, 9, 10),
-	'aeolian'	:(0, 2, 3, 5, 7, 8, 10),
-	'phrygian'	:(0, 1, 3, 5, 7, 8, 10),
-	'locrian'	:(0, 1, 3, 5, 6, 8, 10),
-	'major'		:(0, 2, 4, 5, 7, 9, 11), #same as ionian
-	'minor'		:(0, 2, 3, 5, 7, 8, 10), #same as aeolian
-}
-
-scale_chords_rel = {
-	'lydian':(
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6), #dim
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-	),
-	'ionian':(
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6), #dim
-	),
-	'mixolydian':(
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6), #dim
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-	),
-	'dorian':(
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6), #dim
-		(0, 4, 7),
-	),
-	'aeolian':(
-		(0, 3, 7),
-		(0, 3, 6), #dim
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-	),
-	'phrygian':(
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6), #dim
-		(0, 4, 7),
-		(0, 3, 7),
-	),
-	'locrian':(
-		(0, 3, 6), #dim
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-		(0, 3, 7),
-	),
-	'major':(
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 6),
-	),
-	'minor':(
-		(0, 3, 7),
-		(0, 3, 6),
-		(0, 4, 7),
-		(0, 3, 7),
-		(0, 3, 7),
-		(0, 4, 7),
-		(0, 4, 7),
-	),
-}
-
-scale_chords_abs = {
-	'lydian':(
-		(0, 4, 7),
-		(2, 5, 9),
-		(4, 7, 10),
-		(0, 5, 9),
-		(2, 7, 10),
-		(0, 4, 9),
-		(2, 5, 10),
-	),
-	'ionian':(
-		(0, 4, 7),
-		(2, 5, 9),
-		(4, 7, 11),
-		(0, 5, 9),
-		(2, 7, 11),
-		(0, 4, 9),
-		(2, 5, 11),
-	),
-	'mixolydian':(
-		(0, 4, 7),
-		(2, 5, 9),
-		(4, 7, 10),
-		(0, 5, 9),
-		(2, 7, 10),
-		(0, 4, 9),
-		(2, 5, 10),
-	),
-	'dorian':(
-		(0, 3, 7),
-		(2, 5, 9),
-		(3, 7, 10),
-		(0, 5, 9),
-		(2, 7, 10),
-		(0, 3, 9),
-		(2, 5, 10),
-	),
-	'aeolian':(
-		(0, 3, 7),
-		(2, 5, 8),
-		(3, 7, 10),
-		(0, 5, 8),
-		(2, 7, 10),
-		(0, 3, 8),
-		(2, 5, 10),
-	),
-	'phrygian':(
-		(0, 3, 7),
-		(1, 5, 8),
-		(3, 7, 10),
-		(0, 5, 8),
-		(1, 7, 10),
-		(0, 3, 8),
-		(1, 5, 10),
-	),
-	'locrian':(
-		(0, 3, 6),
-		(1, 5, 8),
-		(3, 6, 10),
-		(0, 5, 8),
-		(1, 6, 10),
-		(0, 3, 8),
-		(1, 5, 10),
-	),
-	'major':(
-		(0, 4, 7),
-		(2, 5, 9),
-		(4, 7, 11),
-		(0, 5, 9),
-		(2, 7, 11),
-		(0, 4, 9),
-		(2, 5, 11),
-	),
-	'minor':(
-		(0, 3, 7),
-		(2, 5, 8),
-		(3, 7, 10),
-		(0, 5, 8),
-		(2, 7, 10),
-		(0, 3, 8),
-		(2, 5, 10),
-	),
-}
-
-PPQN = 24
-
-
-class Sequence(collections.abc.MutableSequence):
-	"""
-	A sequence is a ring list which can be traversed by iterating over it. This
-	iteration does not terminate and the contents of the list are returned
-	repeatedly.
-
-	The sequence may contain arbitrary values. There are a few helper class-
-	methods to deal with certain kinds of values systematically.
-
-	"""
-	def __init__(self, elems, step=1):
-		self.__data__ = list(elems)
-		self._lock = threading.Lock()
-		self._step = step
-		self._iter = self._traverse()
-		self._iter.send(None)
-		#this is due to the fact that the first action on a generator has to be
-		#send(None) or a next() call on the iterator
-		#but the step.setter will call self._iter.send(val) before the first
-		#next() is called
-
+class NoteList:
 	@classmethod
 	def _string_to_note(cls, note):
 		#helper method
@@ -269,74 +31,12 @@ class Sequence(collections.abc.MutableSequence):
 			chord_list.append(note_value)
 		return tuple(chord_list)
 
-	def string_to_note(self):
+	@classmethod
+	def string_to_note(cls, seq):
 		note_list = []
-		for string in self.__data__:
-			note_list.append(self._string_to_note(string))
-		return Sequence(note_list)
-
-	def __len__(self):
-		return len(self.__data__)
-
-	def __getitem__(self, index):
-		return self.__data__[index]
-
-	def __setitem__(self, index, value):
-		##TODO: update self.data as well by reversing _numerize somehow
-		self[index] = value
-
-	def __delitem__(self, index):
-		del self[index]
-		self._cursor -= 1
-
-	def insert(self, index, obj):
-		#insert value before index
-		self.__data__.insert(index, obj)
-
-	def __iter__(self):
-		return self._iter
-
-	def __next__(self):
-		return next(self._iter)
-
-	def _traverse(self):
-		cur = 0 		#cursor position
-		dir = self.step	#step direction
-		yield #to start the generator with a send without skipping the first element
-		while True:
-			while len(self) > 0:
-				i = (yield self[cur])
-				#values can be passed into this with send()
-				#with this, you can change dir while the iterator runs
-				#see: https://docs.python.org/3/howto/functional.html#passing-values-into-a-generator
-				if i == 0:
-					#send(0) will reset the cursor
-					cur = 0
-				elif i is None:
-					cur += dir
-					cur = cur%len(self)
-				else:
-					dir = i
-			yield None
-			#this will yield None until some data is in the Sequence
-
-	def reset(self):
-		self._iter.send(0)
-
-	@property
-	def step(self):
-		return self._step
-
-	@step.setter
-	def step(self, val):
-		if val != 0:
-			self._step = val
-			self._iter.send(val)
-		else:
-			raise ValueError('step cannot be zero!')
-
-	def once(self):
-		return list(self.__data__)
+		for string in seq:
+			note_list.append(cls._string_to_note(string))
+		return note_list
 
 
 class Singleton(type):
@@ -417,7 +117,7 @@ class BaseSequencer(mido.ports.BaseOutput):
 	"""
 	def __init__(
 		self,
-		sequence=Sequence([]),
+		sequence=[],
 		receiver=None,
 		division=16,
 		channel=1,
@@ -425,11 +125,15 @@ class BaseSequencer(mido.ports.BaseOutput):
 	):
 		super().__init__()
 		#for inheriting from BaseOutput
+		#with this, self._lock (threading.Lock object) is available
 
 		self._running = False
 		self._pulses = 0
+		self._cursor = 0
 
 		Timer().add_receiver(self)
+		#set up the sequencer to receive clock messages from the Timer
+
 		self.receiver = receiver
 		#receiver is supposed to be a mido port with a send(msg) method
 
@@ -460,7 +164,7 @@ class BaseSequencer(mido.ports.BaseOutput):
 
 		if notes:
 		#note_to_midi receives notes=None or empty tuple
-		#when a sequence skips a beat
+		#when a sequence skips a beat or is empty
 			for note in notes:
 				message_list.append(
 					mido.Message(
@@ -480,6 +184,13 @@ class BaseSequencer(mido.ports.BaseOutput):
 
 	def start(self):
 		self._running = True
+
+	def advance(self):
+		if len(self.sequence) > 0:
+			current = self.sequence[self._cursor]
+			self._cursor += self.step
+			self._cursor = self._cursor % len(self.sequence)
+			return current
 
 	@property
 	def division(self):
@@ -505,31 +216,13 @@ class BaseSequencer(mido.ports.BaseOutput):
 		else:
 			raise ValueError('Invalid note length: '+str(val))
 
-	@property
-	def step(self):
-		return self._sequence.step
-
-	@step.setter
-	def step(self, val):
-		self._sequence.step = val
-
-	@property
-	def sequence(self):
-		return self._sequence
-
-	@sequence.setter
-	def sequence(self, seq):
-		self._sequence = seq
-		self._seq_iter = iter(seq)
-		self._sequence.step = self.step
-
 	##TODO: set up a sequence property so the new sequence is behaving like the old sequence
 
 
 class StepSequencer(BaseSequencer):
 	def __init__(
 		self,
-		sequence=Sequence([]),
+		sequence=[],
 		receiver=None,
 		division=16,
 		channel=1,
@@ -548,7 +241,7 @@ class StepSequencer(BaseSequencer):
 		#self._lock is inherited from mido.ports.BaseOutput
 		#TODO: has all of this to be locked? Can a subset be savely locked?
 			if self._pulses == 0:
-				self._current_step = next(self._seq_iter)
+				self._current_step = self.advance()
 
 				for note in self.note_to_midi(
 					self._current_step,
@@ -580,8 +273,8 @@ class Arpeggiator(BaseSequencer):
 			channel=channel,
 			step=1
 		)
-		self.rhythm = Sequence([True, True, True, False,])
-		self.chords = Sequence(['cmin','d#maj','gmin'])
+		self.rhythm = [True, True, True, False,]
+		self.chords = ['cmin','d#maj','gmin']
 		self.sequence = self._create_sequence()
 
 	def _send(self, msg):
@@ -595,7 +288,7 @@ class Arpeggiator(BaseSequencer):
 		#self._lock is inherited from mido.ports.BaseOutput
 		#TODO: has all of this to be locked? Can a subset be savely locked?
 			if self._pulses == 0:
-				self._current_step = next(self._seq_iter)
+				self._current_step = self.advance()
 
 				for note in self.note_to_midi(
 					self._current_step,
@@ -629,7 +322,7 @@ class Arpeggiator(BaseSequencer):
 		#translates the chord to notes like 'c#'', 'd', etc.'
 
 		arp_seq = []
-		for chord in self.chords.once():
+		for chord in self.chords:
 			if chord[1] == '#' or chord[1] == 'b':
 				_root_note = chord[:2].lower()
 				_chord_modifier = chord[2:].lower()
@@ -655,55 +348,115 @@ class Arpeggiator(BaseSequencer):
 			for semitones in chord_semitones:
 				notelist.append(note_val + semitones)
 
-			chord_seq = Sequence(notelist)
+			notelist.reverse()
+			#for FIFO access
 
-			for beat in self.rhythm.once():
+			for beat in self.rhythm:
+				cursor = 0
 				if beat:
-					arp_seq.append((next(chord_seq),))
+					#if the sequence of beats contains more beats than the
+					#sequence of notes contains notes, the sequence of notes
+					#will be inserted repeatedly
+					arp_seq.append((notelist[cursor],))
+					cursor += 1
+					cursor = cursor % len(notelist)
 				else:
 					arp_seq.append(tuple())
 
-		return Sequence(arp_seq)
+		return arp_seq
 
-class PrintPort(mido.ports.BaseOutput):
-	def __init__(self):
-		super().__init__()
-	def _send(self, msg):
-		print(msg)
+
 
 if __name__ == '__main__':
-	##TODO: this should be a test
+	import unittest
 
-	sequence1 = Sequence(['c1','c#1','d1','d#1','e1','f1','f#1','g1','g#1','a1','a#1','b1'])
-	print(sequence1.string_to_note())
-	sequence2 = Sequence(['g#4','c5','f4'])
+	class SeqTest(unittest.TestCase):
+		class Port(mido.ports.BaseOutput):
+			def __init__(self):
+				super().__init__()
+			def _send(self, msg):
+				pass
 
-	output_list = mido.get_output_names()
-	print('Select the target port:')
-	for num, name in enumerate(output_list):
-		print('{num}: {name}'.format(num=num, name=name))
+		def setUp(self):
+			self.port = self.Port()
+			self.allNotesSequence = [ \
+				n+ex+str(num) \
+				for n in note_names.keys() \
+				for ex in ['','#','b'] \
+				for num in range(1,11) \
+			]
+			self.allChordsSequence = [
+				x + ' ' + y + ' ' + z + ' ' \
+				for x in self.allNotesSequence[0:26] \
+				for y in self.allNotesSequence[0:26] \
+				for z in self.allNotesSequence[0:26] \
+			]
 
-	port = int(input('(seq) '))
+		def tearDown(self):
+			self.port.close()
 
-	with mido.open_output(mido.get_output_names()[port]) as port:
+		def test_NoteList(self):
+			def checkElements(seq):
+				seq = NoteList.string_to_note(seq)
+				for elem in seq:
+					self.assertIsInstance(elem, tuple)
+					for note in elem:
+						self.assertIsInstance(note, int)
+
+			checkElements(self.allNotesSequence)
+			checkElements(self.allChordsSequence)
+			seq = ['this', 'is', 'a', 'test']
+			with self.assertRaises(KeyError):
+				NoteList.string_to_note(seq)
+			seq = [None]
+			with self.assertRaises(AttributeError):
+				NoteList.string_to_note(seq)
+			seq = [12]
+			with self.assertRaises(AttributeError):
+				NoteList.string_to_note(seq)
+
+		def test_Sequencer(self):
+			seq = BaseSequencer(sequenceself.allNotesSequence, receiver=self.port)
+			seq._send() = lambda x:None
+			with self.assertRfor elem in seq.sequence:
+				seq.advance()
+			seq.advance()
+
+
+
+	unittest.main(verbosity=2)
+
+	sequence2 = NoteList.string_to_note(['g#4','c5','f4'])
+
+	# output_list = mido.get_output_names()
+	# print('Select the target port:')
+	# for num, name in enumerate(output_list):
+	# 	print('{num}: {name}'.format(num=num, name=name))
+	#
+	# port = int(input('(seq) '))
+
+	# with mido.open_output(mido.get_output_names()[port]) as port:
+	with PrintPort() as port:
 		Timer().tempo = 160
-		arp = Arpeggiator(receiver=port)
+		# arp = Arpeggiator(receiver=port)
+		seq = StepSequencer(receiver=port)
+		seq.sequence = sequence2
 		# seq = StepSequencer(receiver=port)
 		# seq.division = 16
 		# seq.sequence = sequence1.string_to_note()
-		# seq.start()
-		arp.start()
-		time.sleep(30)
-		# seq.stop()
+		seq.start()
+		# arp.start()
+		time.sleep(1)
+		seq.stop()
 		#arp.chord = 'dmaj'
 		# time.sleep(5)
 		#arp.chord = 'd#sus2'
 		# time.sleep(5)
-		arp.stop()
+		# arp.stop()
 	# 	seq1 = Sequencer(sequence=sequence1, receiver=synth)
 	# 	seq2 = Sequencer(sequence=sequence2, receiver=synth)
 	# 	seq1.start()
 	# 	seq2.start()
 	# 	time.sleep(5)
-	# 	seq1.stop()
+		# seq1.stop()
 	# 	seq2.stop()
